d052d7470997604f5b62deaca42bbadb88e571d9
roberto189 <126641489+roberto189@users.noreply.github.com>
2023-04-10 11:12:53 -0700

    fixed minor bugs

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
-const characterAPI = //API INFORMATION
-const burgerAPI = //API INFORMATION
+//const characterAPI = //API INFORMATION
+//const burgerAPI = //API INFORMATION
 
 const titleEl = document.getElementById("title");
 const burgerSignEl = document.getElementById("burger-sign");
 const burgerEl = document.getElementById("burger");
-const burgerBtnEl = document.getElementById("burger-btn");
+const burgerBtnEl = document.getElementById("#burger-btn");
 const charInfoEl = document.getElementById("char-info");
-const charBtnEl = document.getElementById("char-btn");
+const charBtnEl = document.getElementById("#char-btn");
 const nameEl = document.getElementById("name");
 const ageEl = document.getElementById("age")
 const firstEpEl = document.getElementById("first-ep");
@@ -16,4 +16,3 @@ const voiceEl = document.getElementById("voice");
 
 burgerBtnEl.addEventListener("click, generateBurgeroftheDay");
 charBtnEl.addEventListener("click, generateCharInfo");
-
