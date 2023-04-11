11f963539befbc8b1c015a78d171bbc533c4ca55
CodingTrophyWife <126922695+CodingTrophyWife@users.noreply.github.com>
2023-04-11 13:39:32 -0700

    added framework

--- assets/JavaScript/fetch.js
+++ assets/JavaScript/fetch.js
@@ -1,8 +1,8 @@
-var requestURL = "https://bobsburgers-api.herokuapp.com/characters/?limit=9&skip=70";
+// var requestURL = "https://bobsburgers-api.herokuapp.com/characters/?limit=9&skip=70";
 
-fetch(requestURL).then(function(response){
-    return response.json ();
-})
-.then(function(data){
-    console.log(data)
-})
+// fetch(requestURL).then(function(response){
+//     return response.json ();
+// })
+// .then(function(data){
+//     console.log(data)
+// })
--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,19 +1,19 @@
-const characterAPI = "https://bobsburgers-api.herokuapp.com/characters/limit=9&skip=70";
-//const burgerAPI = API INFORMATION
+// const characterAPI = "https://bobsburgers-api.herokuapp.com/characters/limit=9&skip=70";
+// //const burgerAPI = API INFORMATION
 
-const titleEl = document.getElementById("title");
-const burgerSignEl = document.getElementById("burger-sign");
-const burgerEl = document.getElementById("burger");
-const burgerBtnEl = document.getElementById("burger-btn");
-const charInfoEl = document.getElementById("char-info");
-const charBtnEl = document.getElementById("char-btn");
-const nameEl = document.getElementById("name");
-const ageEl = document.getElementById("age")
-const firstEpEl = document.getElementById("first-ep");
-const voiceEl = document.getElementById("voice");
+// const titleEl = document.getElementById("title");
+// const burgerSignEl = document.getElementById("burger-sign");
+// const burgerEl = document.getElementById("burger");
+// const burgerBtnEl = document.getElementById("burger-btn");
+// const charInfoEl = document.getElementById("char-info");
+// const charBtnEl = document.getElementById("char-btn");
+// const nameEl = document.getElementById("name");
+// const ageEl = document.getElementById("age")
+// const firstEpEl = document.getElementById("first-ep");
+// const voiceEl = document.getElementById("voice");
 
-//FUNCTIONS
+// //FUNCTIONS
 
-burgerBtnEl.addEventListener("click, generateBurgeroftheDay");
-charBtnEl.addEventListener("click, generateCharInfo");
+// burgerBtnEl.addEventListener("click, generateBurgeroftheDay");
+// charBtnEl.addEventListener("click, generateCharInfo");
 
